f8205612ddca992a6168dcc00249d3fb84e4a2f8
Miguel Cedillo <82360408+migyced@users.noreply.github.com>
2022-12-22 22:28:01 -0800

    add boat image

--- src/Main.js
+++ src/Main.js
@@ -1,10 +1,11 @@
-let config = {
+var config = {
     type: Phaser.CANVAS,
     width: window.innerWidth-30,
     height: window.innerHeight-30,
+    backgroundColor: 'rgb(255, 0, 0)',
     scene: [ Load, Menu, Play ]
 }
 
-let game = new Phaser.Game(config);
+var game = new Phaser.Game(config);
 
 var keyF, keyLEFT, keyRIGHT, keyUP, keyDOWN, keyCLICK;
