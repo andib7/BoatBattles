a7f0c118024e1e71a4dd903ecb49d0f8d372eec1
abarron2 <andibarron7@gmail.com>
2022-12-22 21:46:34 -0800

    movement

--- src/prefabs/Boat.js
+++ src/prefabs/Boat.js
@@ -2,20 +2,23 @@ class Boat extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame) {
         super(scene, x, y, texture, frame);
         scene.add.existing(this);
+
         this.rotation = Math.PI / 2;
         this.speed = 0;
         this.rotationSpeed = 0;
     }
 
     update(time, delta){
-        if(Phaser.Input.Keyboard.JustDown(keyUP)){
-            sprite.x += Math.cos(this.rotation) * this.speed * delta / 1000;
-            sprite.y += Math.sin(this.rotation) * this.speed * delta / 1000;
+        if(keyUP.isDown){
+            //sprite.x += Math.cos(this.rotation) * this.speed * delta / 1000;
+            //sprite.y += Math.sin(this.rotation) * this.speed * delta / 1000;
+            sprite.y += 20;
+            console.log("HI");
         }
-        if(Phaser.Input.Keyboard.JustDown(keyLEFT)){
+        if(this.speed == 1){
 
         }
-        else if(Phaser.Input.Keyboard.JustDown(keyRIGHT)){
+        else if(this.speed == 2){
 
         }
     }
