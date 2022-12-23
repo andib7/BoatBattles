d4560cf0db75bec68eeaac53dd08eb80aaa24b68
Miguel Cedillo <82360408+migyced@users.noreply.github.com>
2022-12-23 13:43:06 -0800

    better boat movement

--- src/prefabs/Boat.js
+++ src/prefabs/Boat.js
@@ -2,30 +2,31 @@ class Boat extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame) {
         super(scene, x, y, texture, frame);
         scene.add.existing(this);
-
-        this.speed = 2;
-        this.rotationSpeed = 1;
+        
+        this.speed = 0.4;
+        this.rotationSpeed = 0.3;
     }
 
     create(){
         this.radians = 0;
+        
     }
 
     update(time, delta){
         this.radians = this.angle*(Math.PI/180);
         if(keyUP.isDown){
-            
             this.x += Math.cos(this.radians) * this.speed;
             this.y += Math.sin(this.radians) * this.speed;
         }
+        if(keyDOWN.isDown){
+            this.x -= Math.cos(this.radians)*this.speed;
+            this.y -= Math.sin(this.radians)*this.speed;
+        }
         if(keyLEFT.isDown){
             this.angle -= this.rotationSpeed;
-
-            console.log(this.angle);
         }
         else if(keyRIGHT.isDown){
             this.angle += this.rotationSpeed;
-            console.log(this.angle);
         }
     }
 
