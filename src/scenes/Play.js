fb673ef726bb0ed5a315f5bc7abb05ed17207e5c
Miguel Cedillo <82360408+migyced@users.noreply.github.com>
2022-12-22 21:52:55 -0800

    .

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -7,7 +7,12 @@ class Play extends Phaser.Scene {
         game.config.backgroundColor = "#ff86dd";
         console.log('Play');
 
-        
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+        keyUP = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
+        keyDOWN = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
+        keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
+        keyCLICK = this.input.keyboard.addKey(Phaser.Input.Mouse.MouseManager.onMouseDown);
 
         this.player = new Boat(this, game.config.width/2, game.config.height/2, 'Boat', 0);
     }
